b432781f32ad42935b2509786759dee647c70b2c
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2023-06-21 13:26:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,3 @@
-import os
-import requests
-from flask import Flask, render_template, url_for, request, redirect, session
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from datetime import datetime
-from dotenv import load_dotenv
-from flask_mail import Mail, Message
-
-load_dotenv()
-
-app = Flask(__name__)
-mail = Mail(app)
-
-if app.config["DEBUG"] == True:
-    app.config.from_object('config.DevConfig')
-else:
-    app.config.from_object('config.ProdConfig')
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-class Faq(db.Model):
-    __tablename__ = 'faq'
-    id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(150))
-    answer = db.Column(db.String(100))
-    timestamp = db.Column(db.String(100))
-
-    def __init__(self, question, answer, timestamp):
-        self.question = question
-        self.answer = answer
-        self.timestamp = timestamp
-
-with app.app_context():
-    db.create_all()
-
-#index route
-@app.route('/')
-def index():
-    data = Faq.query.all()
-    return render_template('index.html', data=data)
-
-#add faq
-@app.route('/add-faq', methods=['GET', 'POST'])
-def addfaq():
-    if request.method == 'POST':
-        question = request.form.get('question')
-        answer = request.form.get('answer')
-        timestamp = datetime.utcnow()
-        
-        data = Faq(question, answer, timestamp)
-        db.session.add(data)
-        db.session.commit()
-
-        return redirect(url_for('index'))
-    return render_template('add-faq.html')
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy
@@ -72,6 +14,7 @@ intents = json.loads(open('application/content.json').read())
 UPLOAD_FOLDER = 'application/static/upload/'
 from dotenv import load_dotenv
 load_dotenv()
+app = Flask(__name__)
 import os
 SECRET_KEY = os.getenv("MY_SECRET")
 app.config.update(dict(
@@ -79,6 +22,9 @@ SECRET_KEY="powerful secretkey",
 WTF_CSRF_SECRET_KEY="dudu rohosio"
     ))
 CORS(app)
+@app.route('/')
+def index():
+  return "<h1>hello world</h1>"
 @app.route('/json')
 def defjson():
   return jsonify({"status":200,"description":"hello world"})
