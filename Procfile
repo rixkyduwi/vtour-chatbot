76d69fade69fb80f078e655edac2ae086d736116
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2023-06-21 10:22:25 +0700

    try to fix deployment error

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn wsgi:app
 heroku ps:scale web=1
+gunicorn --worker-tmp-dir /dev/shm vtour-chatbot.wsgi
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from application import app
-
+import os
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=4040,debug=True)
+    app.run(port=int(os.environ.get("PORT", 5000)))
+    #app.run(host="0.0.0.0",port=4040,debug=True)
